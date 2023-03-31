ec73f76b6b016c536797b47c232bfe6c9970c90c
fpcoutinho <fpcoutinho5@gmail.com>
2023-03-31 10:07:27 -0300

    estrutura do header finalizada

--- src/pages/header.js
+++ src/pages/header.js
@@ -23,13 +23,45 @@ const header = () => {
   });
 
   const html = document.createElement("header");
+  const logoMenu = document.createElement("div");
+  logoMenu.classList.add("logo-menu");
   const logo = document.createElement("img");
   logo.setAttribute("src", logoImg);
   logo.setAttribute("alt", "logo");
-  logo.setAttribute("width", "50rem");
+  const openSideNav = document.createElement("button");
+  openSideNav.innerHTML = `<span class="material-symbols-outlined" id="icone-menu">menu</span>`;
+  const searchBar = document.createElement("form");
+  searchBar.classList.add("search-bar");
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("type", "text");
+  searchInput.setAttribute("placeholder", "Pesquisar");
+  const searchIcon = document.createElement("button");
+  searchIcon.setAttribute("type", "submit");
+  searchIcon.setAttribute("id", "btn-search");
+  searchIcon.innerHTML = `<span class="material-symbols-outlined">search</span>`;
+  searchBar.appendChild(searchInput);
+  searchBar.appendChild(searchIcon);
+  logoMenu.appendChild(openSideNav);
+  logoMenu.appendChild(logo);
+  logoMenu.appendChild(searchBar);
+  html.appendChild(logoMenu);
 
-  html.appendChild(logo);
-  html.appendChild(botaoTema);
+  const headerMenu = document.createElement("div");
+  headerMenu.classList.add("header-menu");
+  const botaoAdd = document.createElement("button");
+  botaoAdd.innerHTML = `<span class="material-symbols-outlined" id="icone-add">add</span>`;
+  const botaoNotificacao = document.createElement("button");
+  botaoNotificacao.innerHTML = `<span class="material-symbols-outlined" id="icone-notificacao">notifications</span>`;
+  const botaoPerfil = document.createElement("button");
+  botaoPerfil.innerHTML = `<span class="material-symbols-outlined" id="icone-perfil">person</span>`;
+
+  headerMenu.appendChild(botaoTema);
+  headerMenu.appendChild(botaoAdd);
+  headerMenu.appendChild(botaoNotificacao);
+  headerMenu.appendChild(botaoPerfil);
+
+  html.appendChild(logoMenu);
+  html.appendChild(headerMenu);
 
   return html;
 };
